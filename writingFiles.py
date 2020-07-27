2a403ccd98dcd3086e35851e061e2c9008ed16ac
JoaoFula <hsifula@gmail.com>
2020-07-27 10:13:04 +0100

    Removed files that weren't in the folder

--- writingFiles.py
+++
@@ -1,6 +0,0 @@
-## This program opens and writes in a text file, remember to add () to the
-#file.close()
-
-file = open("path/textExample.txt", "w")
-file.write("Hi, I am Joao, I am 22 years old")
-file.close()
