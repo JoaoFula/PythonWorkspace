04e80fac4e524013ea7756c5d3f5525356ac89a7
JoaoFula <hsifula@gmail.com>
2020-09-01 10:46:58 +0100

    Small reorganization

--- projects/paint.py
+++ projects/paint.py
@@ -9,19 +9,19 @@ def paint(event):
     python_green = "#476042"
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
-    w.create_oval(x1, y1, x2, y2, fill = python_green)
+    w.create_oval(x1, y1, x2, y2, fill=python_green)
 
 
 master = Tk()
-master.title("Painting using oval") 
+master.title("Painting using oval")
 w = Canvas(master,
-            width = canvas_width,
-            height = canvas_height)
+           width=canvas_width,
+           height=canvas_height)
 
-w.pack(expand = YES, fill = BOTH)
+w.pack(expand=YES, fill=BOTH)
 w.bind("<B1-Motion>", paint)
 
-message = Label(master, text = "Press and drag the mouse to draw ")
-message.pack(side = BOTTOM)
+message = Label(master, text="Press and drag the mouse to draw ")
+message.pack(side=BOTTOM)
 
 mainloop()
